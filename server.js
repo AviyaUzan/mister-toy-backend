0730841c66ea0b273fd608c350a39e60bdfe50f2
Aviya Uzan <aviyauzan@gmail.com>
2022-07-11 19:58:45 +0300

    added a server and a service file

--- server.js
+++ server.js
@@ -1,2 +1,73 @@
 const express = require('express')
+const cors = require('cors') 
 
+const toyService = require('./service/toy.service');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// LIST
+app.get('/api/toy', (req, res) => {
+    // const { title, pageIdx = 0 } = req.query;
+    // query will get filterBy inside
+    // const filterBy = {
+    //     title,
+    //     pageIdx,
+    // };
+    toyService
+        .query()
+        .then((toys) => res.send(toys))
+        .catch((err) => res.status(500).send('Cannot get toys'))
+});
+
+//CREATE
+app.post('/api/toy', (req, res) => {
+    const toy = {
+        name: req.body.name,
+        price: req.body.price,
+        createdAt: req.body.createdAt,
+        inStock: req.body.inStock,
+    }
+    toyService
+        .save(toy)
+        .then((savedToy) => res.send(savedToy))
+        .catch((err) => res.status(500).send('Cannot save toy'));
+});
+
+// UPDATE
+app.put('/api/toy/:toyId', (req, res) => {
+    const toy = {
+        _id: req.body._id,
+        name: req.body.name,
+        price: req.body.price,
+        createdAt: req.body.createdAt,
+        inStock: req.body.inStock,
+    }
+    toyService
+        .save(toy)
+        .then((savedToy) => res.send(savedToy))
+        .catch((err) => res.status(500).send('Cannot save toy'));
+});
+
+//READ
+app.get('/api/toy/:toyId', (req, res) => {
+    const { toyId } = req.params
+
+    toyService.getById(toyId)
+        .then((toy) => {
+            res.send(toy);
+        })
+        .catch((err) => res.status(500).send('Cannot get toy'))
+});
+
+app.delete('/api/toy/:toyId', (req, res) => {
+    const { toyId } = req.params;
+
+    toyService.remove(toyId)
+        .then(() => res.send('Removed!'))
+        .catch((err) => res.status(401).send('Cannot remove toy'))
+});
+
+app.listen(3030, () => console.log('Server ready at port 3030!'));
---
+++ service/toy.service.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const toys = require('../data/toy.json')
+
+const PAGE_SIZE = 3
+
+module.exports = {
+    query,
+    getById,
+    remove,
+    save
+}
+
+function query() {
+    // const regex = new RegExp(filterBy.title,"i")
+    // var filteredToys = toys.filter(toy => regex.test(toy.title)) 
+
+    // const startIdx = filterBy.pageIdx * PAGE_SIZE 
+    // filteredtoys = filteredtoys.slice(startIdx, startIdx + PAGE_SIZE) 
+    return Promise.resolve(toys)
+}
+
+function getById(toyId) {
+    const toy = toys.find(toy => toy._id === toyId)
+    return Promise.resolve(toy)
+}
+
+function remove(toyId) {
+    const idx = toys.findIndex(toy => toy._id === toyId)
+    if (idx === -1) return Promise.reject('No such toy')
+
+    toys.splice(idx, 1)
+    return _saveToysToFile()
+}
+
+function save(toy) {
+    if (toy._id) {
+        const idx = toys.findIndex(currToy => currToy._id === toy._id)
+        if (idx === -1) return Promise.reject('No such toy')
+        toys[idx] = toy
+    } else {
+        toy._id = _makeId()
+        toys.push(toy)
+    }
+    return _saveToysToFile()
+        .then(()=>toy)
+}
+
+
+function _makeId(length=5) {
+    var txt = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for(var i=0; i < length; i++)  {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return txt
+}
+
+function _saveToysToFile() {
+    return new Promise((resolve, reject)=>{
+        const content = JSON.stringify(toys, null, 2)
+        fs.writeFile('./data/toy.json', content, err => {
+            if (err) {
+                console.error(err)
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
